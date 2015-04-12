6f87c5eac4494c0b4726a070ee6ea573c17fb087
faqndo <facuespinosa97@gmail.com>
2015-04-12 09:06:27 -0300

    modificando main.js

--- script/main.js
+++ script/main.js
@@ -1,40 +1,10 @@
 MELI.init({client_id: 5138521300457588});
-var miArreglo = [];
-var userID;
-function initialize(){
-      
-      MELI.login(function(){
-
-        MELI.get('/users/me',{},function(data){
-          swal("Deleted!", "Your imaginary file has been deleted.", "success");
-          userID = data[2].id
-          MELI.get('/users/'+userID+'/items/search?access_token='+MELI.getToken(),{},function(data){
-            var items = data[2].results;
-            var itemsL = items.length;
-            for (var i = 0; i < itemsL ; i++) {
-              MELI.get('/items/' + items[i],{},function(data){
-                var misItems = [];
-                misItems.titulo = data[2].title;
-                misItems.precio = data[2].price;
-               console.log(misItems);
-                miArreglo.push(misItems);
-              });
-            }
-             console.log(miArreglo);
-        });
-            
-           
-          });
-
-      });
-    };
-
 
 document.addEventListener("DOMContentLoaded", function(event){
   setTimeout(function(){
     swal({   
       title: "Quieres loguearte?",   
-      text: "Para poder ayudarte tedras que ingresar con tu usuario",   
+      text: "Para poder ayudarte con las ventas tendras que loguearte",   
       type: "warning",   
       showCancelButton: true,   
       confirmButtonColor: "#DD6B55",   
@@ -59,3 +29,22 @@ document.addEventListener("DOMContentLoaded", function(event){
     });    
   }, 1000);
 });
+
+function initialize(){
+  MELI.login(function(){
+    MELI.get('user/me', {}, function(data){
+      swal('Logueado!', "comenzemos a trabajar", 'success');
+      var userId = data[2].id;
+      MELI.get('/user/' + userId + '/items/search?access_token' + MELI.getToken(), {}, function(data){
+        var items = data[2].results;
+        var itemsL = items.length;
+        for(i=0; i< itemsL; i++){
+          MELI.get('/items/' + items[i], {}, function(data){
+            console.log('Titulo: ' + data[2].title);
+            console.log('Titulo: ' + data[2].title);
+          });
+        }
+      });
+    });
+  });
+}
